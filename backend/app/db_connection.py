4e0b8e11e90932d81b7cd58446c03f5bf09ed9ff
dallatIkes <samy.chaabi0@gmail.com>
2026-01-12 15:49:15 -0500

    added Dockerfile + DB URL

--- backend/app/db_connection.py
+++ backend/app/db_connection.py
@@ -1,9 +1,10 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 from app.database import Base
 
-DATABASE_URL = "sqlite:///./test.db"
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 
