eba4b9952c04f553cf99211179dce68bc22aa203
dallatIkes <samy.chaabi0@gmail.com>
2026-04-09 09:25:09 -0400

    added loadenv

--- backend/app/db_connection.py
+++ backend/app/db_connection.py
@@ -1,8 +1,11 @@
 import os
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 from app.database import Base
+
+load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
